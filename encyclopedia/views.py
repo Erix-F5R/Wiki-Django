354fc8f0c00f5e9fc02bf36b7b3ec780a5ff55b7
Eric <ericpeter.f@gmail.com>
2022-02-01 11:29:08 -0500

    tuesday

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
 
@@ -6,5 +7,11 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def page(request, title):
+        
+        return render(request, "encyclopedia/page.html", {
+        "entry": util.get_entry(title)
     })
 
